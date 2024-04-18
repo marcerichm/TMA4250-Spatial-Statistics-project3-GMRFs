4450be73b2da57ccab428ddadf5e5e93d8e3facc
marcerichm <marcerichm@gmail.com>
2024-04-18 13:39:56 +0200

    none

--- problem_1.R
+++ problem_1.R
@@ -1,15 +1,49 @@
+# Import functions.R
+source("functions.R")
 
 # Import libraries
 library(ggplot2)
-library(rgdal)
+library(sf)
 library(spdep)
 
 # Load Data
-Admin1Graph <- read.delim("/project_files/Admin1Graph.txt")
-Admin2Graph <- read.delim("/project_files/Admin2Graph.txt")
-load("/project_files/Admin1Geography.RData")
-load("/project_files/Admin2Geography.RData")
-DirectEstimates <- read.delim("/project_files/DirectEstimates.txt")
+Admin1Graph <- read.table("Admin1Graph.txt")
+Admin2Graph <- read.table("Admin2Graph.txt")
+Admin1Geography <- load("Admin1Geography.RData")
+Admin2Geography <- load("Admin2Geography.RData")
+DirectEstimates <- read.table("DirectEstimates.txt")
+
+
+# PROBLEM 1
+# a)
+
+# Compute the proportion of non-zero elements and display the sparsity pattern
+
+# Convert N to a matrix if it's not already
+if (!is.matrix(N)) {
+  N <- as.matrix(N)
+}
+
+# Create R from N
+R <- -N  # Set off-diagonal to -1 where N_ij is 1
+diag(R) <- rowSums(N)  # Set diagonal entries to the number of neighbors
+
+# Define the precision parameter, t (example: t = 1)
+t <- 1
+
+# Compute Q
+Q <- t * R
+
+
+
+
+
+
+
+
+
+
+
 
 
 
